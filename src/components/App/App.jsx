f35a8e4dfd91be5c53c32374ea5ab4269261c870
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-10-13 09:40:37 +0300

    redundant snippets of routes have been removed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -18,8 +18,8 @@ export const App = () => {
           <Route index element={<Home/>} />
           <Route path="movies" element={<Movies />} />
           <Route path="movies/:movieId" element={<MovieDetails />} >
-            <Route path="/movies/:movieId/cast" element={<Cast/>} />
-            <Route path="/movies/:movieId/review" element={<Reviews/>} />
+            <Route path="cast" element={<Cast/>} />
+            <Route path="review" element={<Reviews/>} />
           </Route>
           <Route path="notFound" element={<NotFoundPage/>} />
         </Route>
